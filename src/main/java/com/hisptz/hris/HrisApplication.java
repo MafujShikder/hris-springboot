a9964d4745716e0f5019d5bc9fa545fff2de4e08
Eliah7 <eliahmbwilo7@gmail.com>
2018-08-06 16:34:32 +0300

    Basic Spring Boot Application

---
+++ src/main/java/com/hisptz/hris/HrisApplication.java
@@ -0,0 +1,12 @@
+package com.hisptz.hris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrisApplication.class, args);
+    }
+}
