5f9e9ce8b044155b5506056786617540c711dc5c
chingaloEric <echingalo@gmail.com>
2018-10-18 16:59:49 +0300

    added mutation for leavetype  table

--- src/main/java/com/hisptz/hris/Bundles/LeaveTypeBundle/LeaveTypeMutation.java
+++ src/main/java/com/hisptz/hris/Bundles/LeaveTypeBundle/LeaveTypeMutation.java
@@ -1,4 +1,53 @@
 package com.hisptz.hris.Bundles.LeaveTypeBundle;
 
-public class LeaveTypeMutation {
+import com.hisptz.hris.core.Model.main.ModelMutation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LeaveTypeMutation extends ModelMutation<LeaveType> {
+    public LeaveTypeMutation(LeaveTypeRepository leaveTypeRepository){
+        this.leaveTypeRepository = leaveTypeRepository;
+    }
+
+    public LeaveType newLeaveType(Integer fieldId, String name, Integer maximumDays, String limitApplicable, String description, String paymentApplicable){
+        LeaveType leaveType = new LeaveType(fieldId, name, maximumDays, limitApplicable, description,paymentApplicable);
+
+        leaveTypeRepository.save(leaveType);
+        return leaveType;
+    }
+
+    public  Boolean deleteLeaveType(Long id){
+        deleteModel(id, leaveTypeRepository);
+        return true;
+    }
+
+    public LeaveType updateLeaveType(Long id, Integer fieldId, String name, Integer maximumDays, String limitApplicable, String description, String paymentApplicable){
+        LeaveType leaveType = leaveTypeRepository.getOne(id);
+
+        if(fieldId != null){
+            leaveType.setFieldId(fieldId);
+        }
+
+        if(name != null){
+            leaveType.setName(name);
+        }
+
+        if(maximumDays != null){
+            leaveType.setMaximumDays(maximumDays);
+        }
+
+        if(limitApplicable != null){
+            leaveType.setLimitApplicable(limitApplicable);
+        }
+
+        if(paymentApplicable != null){
+            leaveType.setPaymentApplicable(paymentApplicable);
+        }
+
+        if(description != null){
+            leaveType.setDescription(description);
+        }
+
+        return leaveType;
+    }
 }
