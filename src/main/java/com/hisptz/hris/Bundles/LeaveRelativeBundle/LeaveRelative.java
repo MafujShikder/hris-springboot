763703525270ddf9dd0c7d3edf73c5d0987d6ea3
chingaloEric <echingalo@gmail.com>
2018-10-19 06:34:08 +0300

    Added the schema for leaverelative tables

--- src/main/java/com/hisptz/hris/Bundles/LeaveRelativeBundle/LeaveRelative.java
+++ src/main/java/com/hisptz/hris/Bundles/LeaveRelativeBundle/LeaveRelative.java
@@ -17,7 +17,7 @@ public class LeaveRelative {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "leave_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Leave leave;
